b55ad9843fbb0d40ab455a794cacf8a0c157fba1
Rubens Lira <rubenslira1156@gmail.com>
2025-01-28 07:29:10 -0300

    Menu da página home

--- src/views/home.php
+++ src/views/home.php
@@ -1,5 +1,14 @@
 <div class="container">
-    <h2><?= "Olá " . $_SESSION['nome'] ?></h2>
-    <p><a href="index.php?action=user-edit" class="btn edit">Editar</a></p>
-    <p><a href="index.php?action=user-delete" class="btn delete" onclick="return confirm('Você tem certeza que deseja deletar sua conta?')">Deletar</a></p>
+    <article class="user-menu">
+        <h2><?= "Olá " . $_SESSION['nome'] ?></h2>
+        <button type="buttom" class="btn enviar">
+            <a href="index.php?action=product-create">Criar Produtos</a>
+        </button>
+        <button type="buttom" class="btn edit">
+            <a href="index.php?action=user-edit"">Editar Perfil</a>
+        </button>
+        <button type="button" class="btn delete">
+            <a href="index.php?action=user-delete" onclick="return confirm('Você tem certeza que deseja deletar sua conta?')">Deletar Perfil</a>
+        </button>
+    </article>
 </div>
